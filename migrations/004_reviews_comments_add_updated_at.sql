2d4e80d7d9392d19fa0012ca701b682046a8a666
aritify <henrythong93@gmail.com>
2026-05-17 02:16:49 -0700

    Add updated_at column to comments and reviews
    Adds an updated timestamp for comments and reviews in case of edits.

--- migrations/004_reviews_comments_add_updated_at.sql
+++ migrations/004_reviews_comments_add_updated_at.sql
@@ -1,3 +1,5 @@
+-- Adds an updated timestamp for any comments and reviews in case of edits
+
 ALTER TABLE comments
     ADD COLUMN updated_at timestamp default current_timestamp on update current_timestamp AFTER created_at;
 
